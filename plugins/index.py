e3c0cdcfb3569764d9044c4874a4686e949c3ebb
Vysakh <101032366+VysakhTG@users.noreply.github.com>
2022-10-02 20:33:38 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -19,13 +19,13 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-@Client.on_message(filters.command(['index']) & filters.user(ADMINS))
+@Client.on_message(filters.command(['forward']) & filters.user(ADMINS))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/index {from channel first message link} {form channel last message link}")
+        return await message.reply("Use correct format.\nExample <code>/forward {from channel first message link} {form channel last message link}")
     links = message.text.strip().split(" ")
     if len(links) != 3:
-        return await message.reply("Use correct format.\nExample <code>/index https://t.me/kcfilmss/10 https://t.me/kcfilmss/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/forward https://t.me/kcfilmss/10 https://t.me/kcfilmss/20</code>.")
     cmd, first, last = links
     regex = re.compile("(https://)?(t\.me/|telegram\.me/|telegram\.dog/)(c/)?(\d+|[a-zA-Z_0-9]+)/(\d+)$")
     match = regex.match(first)
